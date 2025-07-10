a1889455bff8a60f9ae4737d4f8c86b60e280a83
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-07-10 15:20:23 +0530

    code ad ones

--- ipo_project/ipo_app/admin.py
+++ ipo_project/ipo_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import IPO
 
-# Register your models here.
+@admin.register(IPO)
+class IPOAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'status', 'open_date', 'close_date', 'listing_date')
+    search_fields = ('company_name',)
+    list_filter = ('status',)
