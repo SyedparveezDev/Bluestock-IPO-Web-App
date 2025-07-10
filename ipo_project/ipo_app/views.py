eff614ba771816e2320f3ca420112e446d66bc7f
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-07-10 15:23:13 +0530

    views.py add

--- ipo_project/ipo_app/views.py
+++ ipo_project/ipo_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import generics, filters
+from .models import IPO
+from .serializers import IPOSerializer
 
-# Create your views here.
+# API views
+class IPOListAPI(generics.ListAPIView):
+    queryset = IPO.objects.all()
+    serializer_class = IPOSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['company_name', 'status']
+    ordering_fields = ['open_date', 'listing_date']
+
+class IPODetailAPI(generics.RetrieveAPIView):
+    queryset = IPO.objects.all()
+    serializer_class = IPOSerializer
+
+# Web views
+def home(request):
+    ipos = IPO.objects.all()
+    return render(request, 'home.html', {'ipos': ipos})
+
+def ipo_detail(request, pk):
+    ipo = get_object_or_404(IPO, pk=pk)
+    return render(request, 'detail.html', {'ipo': ipo})
