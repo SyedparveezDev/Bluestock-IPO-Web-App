a0139014e6a863bfbf8a612f41df405f4f18b8cb
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-07-10 15:27:32 +0530

    Add Code

---
+++ ipo_project/ipo_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # API endpoints
+    path('api/ipo/', views.IPOListAPI.as_view(), name='api-ipo-list'),
+    path('api/ipo/<int:pk>/', views.IPODetailAPI.as_view(), name='api-ipo-detail'),
+
+    # Web pages
+    path('', views.home, name='home'),
+    path('ipo/<int:pk>/', views.ipo_detail, name='ipo-detail'),
+]
