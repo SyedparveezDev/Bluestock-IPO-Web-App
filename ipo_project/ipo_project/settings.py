b35e9c616d2413293cce9bba9db2d7f7bb465581
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-07-12 18:00:47 +0530

    adding media files

--- ipo_project/ipo_project/settings.py
+++ ipo_project/ipo_project/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / 'ipo_app/static' ]
 
+# Media files (uploaded logos, PDFs)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
