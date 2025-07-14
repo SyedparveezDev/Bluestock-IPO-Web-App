8f884069d9d60644c5e7aab49883c1b37080a09b
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-07-15 00:26:45 +0530

    up date code

--- ipo_project/ipo_app/serializers.py
+++ ipo_project/ipo_app/serializers.py
@@ -1,7 +1,11 @@
-from rest_framework import serializers
+from rest_framework import generics
 from .models import IPO
+from .serializers import IPOSerializer
 
-class IPOSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = IPO
-        fields = '__all__'
+class IPOListAPI(generics.ListAPIView):
+    queryset = IPO.objects.all()
+    serializer_class = IPOSerializer
+
+class IPODetailAPI(generics.RetrieveAPIView):
+    queryset = IPO.objects.all()
+    serializer_class = IPOSerializer
