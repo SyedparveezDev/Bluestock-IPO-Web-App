8beb9e13fde9bdc617434c808edbe81e8c4c4aed
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-07-14 23:25:13 +0530

    adding data base

--- ipo_project/ipo_project/settings.py
+++ ipo_project/ipo_project/settings.py
@@ -75,12 +75,7 @@ WSGI_APPLICATION = 'ipo_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
