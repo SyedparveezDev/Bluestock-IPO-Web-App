45dbff513b98a6e35620154625607d0215ae5c41
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-07-14 18:01:27 +0530

    update

--- ipo_project/ipo_app/views.py
+++ ipo_project/ipo_app/views.py
@@ -26,4 +26,11 @@ def ipo_detail(request, pk):
 
 def upcoming_ipo(request):
     ipos = IPO.objects.filter(status='upcoming')
-    return render(request, 'ipo_app/upcoming_ipo.html', {'ipos': ipos})
+    selected_ipo = None
+    ipo_id = request.GET.get('ipo_id')
+    if ipo_id:
+        selected_ipo = get_object_or_404(IPO, pk=ipo_id)
+    return render(request, 'ipo_app/upcoming_ipo.html', {
+        'ipos': ipos,
+        'selected_ipo': selected_ipo
+    })
