d158162066edbdba98ddad0c05b5ad67dfe5d574
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-07-14 22:59:01 +0530

    new code

--- ipo_project/ipo_app/views.py
+++ ipo_project/ipo_app/views.py
@@ -16,3 +16,13 @@ class IPODetailAPI(generics.RetrieveAPIView):
     serializer_class = IPOSerializer
 
 # Web views
+def upcoming_ipo(request):
+    ipos = IPO.objects.filter(status='upcoming')
+    selected_ipo = None
+    ipo_id = request.GET.get('ipo_id')
+    if ipo_id:
+        selected_ipo = get_object_or_404(IPO, pk=ipo_id)
+    return render(request, 'ipo_app/upcoming_ipo.html', {
+        'ipos': ipos,
+        'selected_ipo': selected_ipo
+    })
