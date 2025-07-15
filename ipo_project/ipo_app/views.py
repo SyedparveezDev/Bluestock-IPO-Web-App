f2d0181e0f55493ccca7eb06447846ce645f3432
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-07-15 15:42:53 +0530

    update new code

--- ipo_project/ipo_app/views.py
+++ ipo_project/ipo_app/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
 from django.core.paginator import Paginator
-from .models import IPO
 
+def home(request):
+    query = request.GET.get('q')
+    status_filter = request.GET.get('status')
 
+    ipos = IPO.objects.all()
 
+    if query:
+        ipos = ipos.filter(company_name__icontains=query)
+    if status_filter:
+        ipos = ipos.filter(status=status_filter)
+
+    paginator = Paginator(ipos, 6)  # 6 IPOs per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'ipo_app/ipo_list.html', {
+        'page_obj': page_obj,
+        'query': query,
+        'status_filter': status_filter,
+    })
